0edb97e22569b18dc268f216b60566d5c0523358
Edgar Romero <80496032+allthingsfrogs@users.noreply.github.com>
2025-07-04 17:58:10 -0700

    put config.py into gitignore

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # WideSwing
+
 Live Valorant Esports Updates via Telegram/SMS
+
+#tentaive work flow:
+Entry Point: /start on telegram WideSWing bot
+
+    Bot prompts for match info, does polling for new match request
+
+    once match is determined, passes this info to python producer
+
+    Producer does polling on vlrggAPI for match updates
+
+    When an update is detected
--- matchProducer.py
+++ matchProducer.py
@@ -1,1 +1,54 @@
-#! /usr/bin./env python3
+#! /usr/bin/env python
+
+import logging
+import sys
+import requests
+import json
+
+from fastapi import FastAPI
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.serialization import StringSerializer
+from confluent_kafka import SerializingProducer
+from config import config
+from confluent_kafka.schema_registry.avro import AvroSerializer
+
+
+from typing import Final
+TOKEN: Final = '7710093996:AAGihZkNJEfVe2jrlO6lWMCrdgjiH2wJf9c'
+BOT_NAME: Final = '@WideSwing'
+
+# send message to user, give choice of VCT region
+# Americas, EMEA, APAC, CN
+def region_selection():
+    region = input("Enter region of the match you want to follow:")
+    return region
+
+
+#retrieve live match data including teams, map score, map count, top frags  
+def fetch_live_match_data():
+    response = requests.get("https://vlrggapi.vercel.app/match?q=upcoming")
+    
+    payload = json.loads(response.text)
+    logging.debug("GOT %s", payload)
+    return payload
+
+# send message to user with live match summary
+def live_summary(fetch_live_match_data):
+    return {
+        "team1": "Team A",
+        "team2": "Team B",
+        "map_score": "13-11",
+        "map_count": 3
+    }
+
+
+def main():
+    match_region = region_selection()
+    #logging.info("GOT %s", match_region)
+    r = fetch_live_match_data()
+    print("Live Match Data:", r)
+
+if __name__ == "__main__":    
+    logging.basicConfig(level=logging.INFO)
+    sys.exit(main())
+
